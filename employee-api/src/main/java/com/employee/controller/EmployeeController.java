a08eba531835c42e9413d957753f83c1ee44d52c
Md.Samiul Arafin <sami>
2021-04-26 23:01:21 +0600

    spring cloud gateway added

--- employee-api/src/main/java/com/employee/controller/EmployeeController.java
+++ employee-api/src/main/java/com/employee/controller/EmployeeController.java
@@ -23,6 +23,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -47,6 +48,7 @@ import lombok.RequiredArgsConstructor;
 @EnableEurekaClient
 @RequiredArgsConstructor
 @Api(tags = "Employees's Data")
+@RequestMapping("/employee")
 public class EmployeeController {
 
 	private final EmployeeService service;
